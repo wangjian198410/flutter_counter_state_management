6e13811673d2cc1c30f2369f8cc52c6663a0df67
JerryWang <Jerry.Wang2@walmart.com>
2020-05-30 19:51:29 +0800

    Revert "change to StatelessWidget"
    This reverts commit 138e2f55646e1976a080eccda002acebea09d55d.

--- state_management/lib/main.dart
+++ state_management/lib/main.dart
@@ -17,16 +17,21 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
 
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     final _bloc = CounterBloc();
     return Scaffold(
         appBar: AppBar(
-          title: Text(title),
+          title: Text(widget.title),
         ),
         body: Center(
           child: Column(
